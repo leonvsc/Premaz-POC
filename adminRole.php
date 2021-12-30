dee3059016a58f1b2a569974de0b7e6c14456ce9
Leon <leonvsc@users.noreply.github.com>
2021-12-30 16:58:07 +0100

    Added the class adminRole in adminRole.php

---
+++ adminRole.php
@@ -0,0 +1,35 @@
+<?php
+
+class adminRole
+{
+    private string $role;
+
+    public function __construct($role)
+    {
+        $this->role = $role;
+    }
+
+    public function viewUser(): bool
+    {
+        return true;
+    }
+
+    public function editUser(): bool
+    {
+        return true;
+    }
+
+    public function deleteUser(): void
+    {
+    }
+
+    public function viewBill(): bool
+    {
+        return true;
+    }
+
+    public function viewOrder(): bool
+    {
+        return true;
+    }
+}
